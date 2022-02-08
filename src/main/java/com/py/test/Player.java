52508a3d8f1c3c630667c1ccc4b37c7ba54822eb
0502lian <0502lian@users.noreply.github.com>
2022-02-08 22:46:38 +0800

    Add files via upload
    add package

---
+++ src/main/java/com/py/test/Player.java
@@ -0,0 +1,43 @@
+package com.py.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: py
+ */
+public class Player {
+
+    private String name;
+    private List<Integer> inputNums = new ArrayList<Integer>();
+    private Integer score = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Integer> getInputNums() {
+        return inputNums;
+    }
+
+    public void addInputNum(Integer input) {
+        inputNums.add(input);
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public void reset(){
+        inputNums.clear();
+        score = 0;
+    }
+}
---
+++ src/main/java/com/py/test/main.java
@@ -0,0 +1,14 @@
+package com.py.test;
+
+import java.util.Scanner;
+
+/**
+ * @Author: py
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Game game = new Game(5, 5);
+        game.prepareAndPlayGames(new Scanner(System.in));
+    }
+}
